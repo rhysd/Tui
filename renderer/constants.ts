f0d6569f7b028c33fdc4812c53a60aab4faf42d4
rhysd <lin90162@yahoo.co.jp>
2017-01-09 20:04:52 +0900

    separate processing webview into webview.ts

--- renderer/constants.ts
+++ renderer/constants.ts
@@ -1,4 +1,5 @@
 export const IS_DEBUG = process.env.NODE_ENV === 'development';
 export const IS_DARWIN = process.platform === 'darwin';
 export const USERAGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36';
+export const DEFAULT_HOME_URL = 'https://mobile.twitter.com';
 
---
+++ renderer/webview.ts
@@ -0,0 +1,51 @@
+import {USERAGENT} from './constants';
+
+export default class WebView {
+    private elem: Electron.WebViewElement;
+    private mounted = false;
+
+    get element() {
+        return this.elem;
+    }
+
+    get contents() {
+        return this.elem.getWebContents();
+    }
+
+    constructor() {
+        const wv = document.createElement('webview');
+        wv.id = 'main-webview';
+        wv.setAttribute('useragent', USERAGENT);
+        wv.setAttribute('partition', 'persist:tuitter');
+        wv.setAttribute('autosize', 'on');
+
+        wv.addEventListener('new-window', e => {
+            e.preventDefault();
+            wv.src = e.url;
+        });
+        wv.addEventListener('crashed', () => {
+            log.error('Webview crashed! Reload <webview> to recover.');
+        });
+        this.elem = wv;
+    }
+
+    mountTo(parent: HTMLElement) {
+        if (this.mounted) {
+            throw new Error('<webview> is already mounted');
+        }
+        parent.appendChild(this.elem);
+        this.mounted = true;
+    }
+
+    openURL(url: string) {
+        return new Promise<void>(resolve => {
+            const resolver = () => {
+                this.elem.removeEventListener('dom-ready', resolver);
+                resolve();
+            };
+            this.elem.addEventListener('dom-ready', resolver);
+            this.elem.src = url;
+        });
+    }
+}
+
