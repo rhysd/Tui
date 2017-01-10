1c136b754d888cb1276437c3952f6ca7d9dd2d72
rhysd <lin90162@yahoo.co.jp>
2017-01-10 13:48:49 +0900

    make classes for each responsibilities

--- webview/index.ts
+++ webview/index.ts
@@ -1,5 +1,11 @@
+import {EventEmitter} from 'events';
+
+const ClassNames = {
+    tweet: '_1eF_MiFx',
+};
+
 const SELECTORS = {
-    tweet: 'div._1eF_MiFx[role="row"]',
+    tweet: `div.${ClassNames.tweet}[role="row"]`,
     notificationCount: '.Fe7ul3Lt.Z_PqXzzk._2DggF3sL._2izplv41',
 };
 
@@ -20,45 +26,106 @@ function doPollingForElementExists(selector: string, retry: number) {
     });
 }
 
-type TweetMutatedCallback = (node: HTMLDivElement) => void;
-
-function watchTweets(cb: TweetMutatedCallback) {
-    // Timeout when no tweet found within 200ms * 25 = 5sec
-    doPollingForElementExists(SELECTORS.tweet, 25).then(tw => {
-        const parent = tw.parentNode;
-        if (parent === null) {
-            console.log('Tui: No parent found for:', tw);
-            return;
-        }
-
-        const observer = new MutationObserver(mutations => {
-            for (const m of mutations) {
-                for (const n of m.addedNodes) {
-                    cb(n as HTMLDivElement);
-                }
-            }
-        });
-
-        observer.observe(parent, {
-            childList: true,
-        });
-    });
-}
-
+// TODO:
+// Make PluginManager
 // TODO:
 // Get list of plugin files via IPC
 // TODO:
 // Implement loading plugin
 
 function applyFilterPlugin(tweet: HTMLDivElement) {
-    console.error('TODO: Apply plugin to tweet element:', tweet);
+    console.error('Tui: TODO: Apply plugin to tweet element:', tweet);
+}
+
+class TweetWatcher extends EventEmitter {
+    observer: MutationObserver | null = null;
+
+    constructor() {
+        super();
+    }
+
+    start(timelineRoot: HTMLDivElement) {
+        if (this.observer !== null) {
+            console.error('Tui: Already watching tweets:', this.observer);
+            return;
+        }
+
+        const observer = new MutationObserver(mutations => {
+            const tweetClassName = ClassNames.tweet;
+            for (const m of mutations) {
+                for (const n of m.addedNodes) {
+                    if ((n as HTMLDivElement).className === tweetClassName) {
+                        this.emit('added', n);
+                    }
+                }
+                for (const n of m.removedNodes) {
+                    if ((n as HTMLDivElement).className === tweetClassName) {
+                        this.emit('removed', n);
+                    }
+                }
+            }
+        });
+
+        observer.observe(timelineRoot, {
+            childList: true,
+        });
+
+        this.observer = observer;
+    }
+
+    isWatching() {
+        return this.observer !== null;
+    }
+}
+
+class AppContext {
+    location = location;
+    timelineRoot: HTMLDivElement | null = null;
+    tweetWatcher = new TweetWatcher();
+
+    isHomeTimeline() {
+        return this.location.pathname === '/home';
+    }
+
+    isMentionTimeline() {
+        return this.location.pathname === '/notifications';
+    }
+
+    isMessagesPage() {
+        return this.location.pathname.startsWith('/messages');
+    }
+
+    setTimelineRoot(root: HTMLDivElement) {
+        this.timelineRoot = root;
+        this.tweetWatcher.start(root);
+    }
+}
+
+function dispatchContext() {
+    const ctx = new AppContext();
+    if (ctx.isMessagesPage()) {
+        return Promise.resolve(ctx);
+    }
+
+    return doPollingForElementExists(SELECTORS.tweet, 25).then(tw => {
+        const parent = tw.parentNode;
+        if (parent === null) {
+            console.error('Tui: No parent found for:', tw);
+            return ctx;
+        }
+
+        ctx.setTimelineRoot(parent as HTMLDivElement);
+        return ctx;
+    });
 }
 
 const handler = () => {
     switch (document.readyState) {
         case 'interactive': {
             console.log('Tui: Reached to "interactive" state. Will inject codes');
-            watchTweets(applyFilterPlugin);
+            dispatchContext().then(ctx => {
+                ctx.tweetWatcher.on('added', applyFilterPlugin);
+            });
             // Note: Ensure to run this clause once
             document.removeEventListener('readystatechange', handler);
             break;
