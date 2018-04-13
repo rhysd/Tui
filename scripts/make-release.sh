a881b850699f5327bf41dce810cb7c5c2d380276
rhysd <lin90162@yahoo.co.jp>
2018-04-14 03:19:24 +0900

    improve release script with shellcheck

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -21,22 +21,24 @@ function prepare-app() {
 }
 
 function pack-app() {
-    local version=$(./bin/cli.js --version)
-    local electron_version=$(electron --version)
-    electron_version=${electron_version#v}
+    local version electron_version
+    version="$(./bin/cli.js --version)"
+    electron_version="$(electron --version)"
+    electron_version="${electron_version#v}"
 
-    electron-packager ./app --platform=darwin --arch=x64 "--app-copyright=copyright (c) 2016 rhysd" --app-version=$version --build-version=$version --icon=./resources/icon.icns --version=$electron_version --no-prune
-    electron-packager ./app --platform=linux --arch=all "--app-copyright=copyright (c) 2016 rhysd" --app-version=$version --build-version=$version --icon=./resources/icon.ico --version=$electron_version --no-prune
-    electron-packager ./app --platform=win32 --arch=all "--app-copyright=copyright (c) 2016 rhysd" --app-version=$version --build-version=$version --icon=./resources/icon.ico --version=$electron_version --version-string=$version --no-prune
+    electron-packager ./app --platform=darwin --arch=x64 "--app-copyright=copyright (c) 2016 rhysd" --app-version="$version" --build-version="$version" --icon=./resources/icon.icns --version="$electron_version" --no-prune
+    electron-packager ./app --platform=linux --arch=all "--app-copyright=copyright (c) 2016 rhysd" --app-version="$version" --build-version="$version" --icon=./resources/icon.ico --version="$electron_version" --no-prune
+    electron-packager ./app --platform=win32 --arch=all "--app-copyright=copyright (c) 2016 rhysd" --app-version="$version" --build-version="$version" --icon=./resources/icon.ico --version="$electron_version" --version-string="$version" --no-prune
 }
 
 function make-dist() {
+    local version
     if [ -d dist ]; then
         rm -rf dist
     fi
     mkdir dist
-    local version="$(./bin/cli.js --version)"
-    for dir in `ls -1 | grep '^Tui-'`; do
+    version="$(./bin/cli.js --version)"
+    for dir in $(ls -1 | grep '^Tui-'); do
         mv "$dir/LICENSE" "$dir/LICENSE.electron"
         cp LICENSE README.md "$dir"
         zip --symlinks "dist/${dir}-${version}.zip" -r "$dir"
